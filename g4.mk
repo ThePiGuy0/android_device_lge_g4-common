fe11b711c0f17f68eec26290e0581df49f8fb544
ThePiGuy <thepiguy0@gmail.com>
2019-07-20 16:08:58 +0100

    Revert "g4: Initial bringup OSS camera HAL"
    This reverts commit 75c0f8e8b9d14a40ba077ca12fdfd51af2550037.

--- g4.mk
+++ g4.mk
@@ -66,7 +66,6 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    libqomx_core \
     libmm-qcamera
 
 # rootless torch tile workaround
